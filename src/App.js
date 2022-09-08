34636d1b24b009a60cfbb4515e0343c705515ce7
DerekKirschbaum <dgkirschbaum@gmail.com>
2022-09-07 21:38:55 -0700

    Basic framework

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App() {
+export default function App () {
   return (
     <div className="App">
-      <h1>Testing</h1>
-    </div>
-  );
-}
+      <div className="info">
+        <h1 className="title">Pyramid Nim</h1>
+        <p className="instructions">Players alternate turns, crossing off as many 
+          adjacent lines in the same row as they want. 
+          The winner is the player that crosses off the last line.</p>
+      </div>
+      <div className="gamebox">
+        <button><img className="tallys" src={require('./pngtallys.png')} alt="tally marks"></img></button>
+      </div>
+      
 
-export default App;
+    </div>
+
+  );
+};
