fff9829d0e25195c4c2eb76c672b5f74d13d5743
Kirschbaum <derekk14720@stu.powayusd.com>
2022-09-08 14:20:33 -0700

    test line element

--- src/App.js
+++ src/App.js
@@ -9,6 +9,26 @@ export default function App () {
       </div>
       <div className="gamebox">
         <Line />
+        <div>
+            <button>
+                <img className="tallys" 
+                    src={require('./images/BlueCrossedLine.png')} 
+                    alt="Blue Crossed Line">
+                </img>
+            </button>
+            <button>
+                <img className="tallys" 
+                    src={require('./images/RedCrossedLine.png')} 
+                    alt="Red Crossed Line">
+                </img>
+            </button>
+            <button>
+                <img className="tallys" 
+                    src={require('./images/BlackLine.png')} 
+                    alt="Black Line">
+                </img>
+            </button>
+        </div>
       </div>
       
 
